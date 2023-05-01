3d570abb00ddf7010924b69726918dec8e69d28a
fahturraman <130740073+fahturraman@users.noreply.github.com>
2023-05-01 12:46:01 +0700

    Add files via upload

---
+++ tugas4/Interface.php
@@ -0,0 +1,36 @@
+<?php
+ interface Peserta {
+ public function dataDiri();
+ public function dataOrtu();
+ public function dataSekolahAsal();
+ }
+ class Siswa implements Peserta {
+ public string $nama = "Fahtur";
+ public function dataDiri() {
+ return $this->nama;
+ }
+
+ public function dataOrtu() {
+ //
+ }
+
+ public function dataSekolahAsal() {
+ //
+ }
+}
+ class Mahasiswa implements Peserta {
+ public string $nama = "Rahman";
+ public function dataDiri() {
+ return $this->nama;
+ }
+ public function dataOrtu() {
+//
+ }
+public function dataSekolahAsal() {
+ //
+ }
+ }
+ $ssw = new Siswa;
+ echo "<p>" . $ssw->dataDiri() . "</p>";
+$mhsw = new Mahasiswa;
+echo "<p>" . $mhsw->dataDiri() . "</p>";
